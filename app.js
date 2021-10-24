726d04b953eb93cbffc511bd9739ef54ffbbda5b
Cooney <ahwhw3@splm.siemens.com>
2021-10-24 07:28:50 +0200

    added authenticate for sequelize

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 // load modules
 const express = require('express');
 const morgan = require('morgan');
+const { sequelize } = require('./models');
 
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
@@ -43,6 +44,15 @@ app.use((err, req, res, next) => {
 app.set('port', process.env.PORT || 5000);
 
 // start listening on our port
-const server = app.listen(app.get('port'), () => {
-  console.log(`Express server is listening on port ${server.address().port}`);
-});
+(async () => {
+  try {
+      await sequelize.authenticate();
+      console.log('Connection has been established successfully');
+  } catch(error) {
+      console.log('Unable to connect to the database', error);
+  }
+})();
+
+// const server = app.listen(app.get('port'), () => {
+//   console.log(`Express server is listening on port ${server.address().port}`);
+// });
